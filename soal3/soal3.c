6b977bf4e865ffb619c5553f597bae575ac005ac
dws-in <61307469+dws-in@users.noreply.github.com>
2020-04-10 16:04:13 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -27,16 +27,15 @@ int isFile(char *path){
 void *cat(void *arg){
   char fileN[50],ext[10],oldP[100],newP[100],dirE[100];
   strcpy(fileN,(char *)arg);
-  printf("%s\n",fileN);
-  sprintf(oldP,"%s%s",old,fileN); printf("%s\n",oldP);
+  sprintf(oldP,"%s%s",old,fileN); 
 
   if(isFile(oldP)) {
-    strcpy(ext,getExt(fileN)); printf("%s\n",ext);
+    strcpy(ext,getExt(fileN)); 
 
     sprintf(dirE,"%s%s",drit,ext);
     DIR* dir = opendir(dirE);
     if(dir==0) {
-      mkdir(dirE,0777);  printf("%s\n",dirE);
+      mkdir(dirE,0777); 
     }
 
     sprintf(newP,"%s%s/%s",new,ext,fileN);
@@ -47,16 +46,15 @@ void *cat(void *arg){
 
 int main(int argc, char *argv[]) {
   int x,err; char file[50];
-  printf("11\n");
 
   if(strcmp(argv[1],"-f")==0) {
-    int n=0; strcpy(old,""); strcpy(new,""); strcpy(drit,""); printf("12\n");
-    while(n != (argc-2)) { printf("13\n");
+    int n=0; strcpy(old,""); strcpy(new,""); strcpy(drit,""); 
+    while(n != (argc-2)) { 
       err=pthread_create(&tid[n],NULL,&cat,(void *)argv[n+2]);
       n++;
     }
-    n=0; printf("14\n");
-    while(n != (argc-2)) { printf("15\n");
+    n=0; 
+    while(n != (argc-2)) {
        pthread_join(tid[n],NULL);
        n++;
     }
@@ -64,15 +62,15 @@ int main(int argc, char *argv[]) {
   }
 
   else if(strcmp(argv[1],"-d")==0) {
-    DIR* dir = opendir(argv[2]);  printf("12\n");
+    DIR* dir = opendir(argv[2]); 
     struct dirent *d; int n=0;
     sprintf(old,"%s/",argv[2]);  strcpy(new,""); strcpy(drit,"");
-    while((d = readdir(dir))!=NULL) { printf("13\n");
+    while((d = readdir(dir))!=NULL) { 
       err=pthread_create(&tid[n],NULL,&cat,(void *)d->d_name);
       n++;
     }
-    printf("14\n");
-    while(n) { printf("15\n");
+    
+    while(n) { 
        pthread_join(tid[n-1],NULL);
        n--;
     }
@@ -85,12 +83,12 @@ int main(int argc, char *argv[]) {
     DIR* dir = opendir(old);
     struct dirent *d; int n=0;
     sprintf(old,"%s/",old);  strcpy(new,old); strcpy(drit,old);
-    while((d = readdir(dir))!=NULL) { printf("13\n");
+    while((d = readdir(dir))!=NULL) { 
       err=pthread_create(&tid[n],NULL,&cat,(void *)d->d_name);
       n++;
     }
-    printf("14\n");
-    while(n) { printf("15\n");
+    
+    while(n) { 
        pthread_join(tid[n-1],NULL);
        n--;
     }
