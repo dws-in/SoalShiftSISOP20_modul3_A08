f5b5ada60d4a4d9107af68bad303cf860e662a3a
dws-in <61307469+dws-in@users.noreply.github.com>
2020-04-11 19:55:20 +0700

    Update 4a.c

--- soal4/4a.c
+++ soal4/4a.c
@@ -1,80 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <unistd.h>
-int (*hasil)[10];
+#include <sys/stat.h>
 
-void *matriks(void *arg)
+#define M 4
+#define K 2
+#define N 5
+
+void *multiply (void *param);
+
+int A [M][K] = { {3,1}, {2,1}, {1,2}, {1,1} }; // Array ukuran M*K (4 baris 2 kolom)
+int B [K][N] = { {1,5,2,1,4}, {10,5,12,1,1} }; // Array ukuran K*N (2 baris 5 kolom)
+int (*C) [M][N]; // Array ukuran M*N (4 baris 5 kolom)
+
+/* Structure untuk mem-passing data ke thread */
+struct v
 {
-    int matriks1[10][10];
-    int matriks2[10][10];
-    
-    int i;
-    int j;
-    //printf("matriks 1 : \n"); 4x2
-    
-	matriks1[0][0]=1;
-	matriks1[0][1]=2;
-
-	matriks1[1][0]=2;
-	matriks1[1][1]=4;
-
-	matriks1[2][0]=1;
-	matriks1[2][1]=2;
-
-	matriks1[3][0]=3;
-	matriks1[3][1]=1;
-	   
-
-    //printf("matriks 2: \n"); 2x5
-    	matriks2[0][0]=1;
-	matriks2[0][1]=2;
-	matriks2[0][2]=0;
-	matriks2[0][3]=4;
-	matriks2[0][4]=1;
-
-	matriks2[1][0]=3;
-	matriks2[1][1]=1;
-	matriks2[1][2]=2;
-	matriks2[1][3]=0;
-	matriks2[1][4]=1;
-    
-    int h;
-    int jumlah=0;
-    //hasil[i][j]=0;
-    for(i = 0; i <= 3; i++){
-        for(j = 0; j <= 4; j++){
-	    //hasil[i][j]=0;
-            for(h = 0; h <= 1; h++){
-                jumlah = jumlah + (matriks1[i][h] * matriks2[h][j]);
-            }
-		hasil [i][j] = jumlah;
-        }
-    }
-	return (*hasil);
-}
+    int i; // Menunjukkan baris
+    int j; // Menunjukkan kolom
+};
 
 int main()
 {
-	key_t key = 8808;
-	int shmid = shmget (key, sizeof(int[10][10]), IPC_CREAT | 0666);
-	hasil = shmat(shmid,0,0);
-	
-	if(shmid<0)
-	{
-		perror("shmget");
-		exit(1)
-	)
-			
-	int i;
-	int j;
-
-    printf("Hasil perkalian matriks: \n");
-    for(i = 0; i < 4; i++){
-        for(j = 0; j < 5; j++){
-            printf("%d\t", hasil[i][j]);
+    
+    // shared memory tempelate
+    key_t key=1234;
+    int shmid = shmget(key, sizeof (int[20]), IPC_CREAT | 0666);
+    C = shmat(shmid,0,0);
+    
+    pthread_t tid [M*N]; // Array of thread bernama tid dengan size M*N = 4*5
+    int i; // Menunjukkan baris
+    int j; // Menunjukkan kolom
+    int indeks=0; // Menunjukkan indeks pada array of thread
+    
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            struct v *data = (struct v*) malloc (sizeof (struct v));
+            /* Membuat objek structure v bernama data
+             dengan mengalokasikan memori sebesar size dari structure v */
+            
+            data -> i = i; // Mengeset nilai i pada objek structure v bernama data dengan nilai i
+            data -> j = j; // Mengeset nilai j pada objek structure v bernama data dengan nilai j
+            
+            pthread_create (&tid[indeks++], NULL, multiply, (void *) data);
+            /* Membuat thread pada array of thread tid pada indeks ke-indeks
+             dengan memanggil fungsi multiply dengan parameter data */
         }
-        printf("\n");
+    }
+    for (indeks=0; indeks < M*N; indeks++)
+    {
+        pthread_join(tid[indeks],NULL);
+    }
+    
+    
+    /* Mencetak hasil perkalian 2 Matriks */
+    printf ("Hasil:\n");
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            printf ("%d\t", *C[i][j]); // Output matriks C baris i kolom j
+        }
+        printf ("\n");
+    }
+    shmdt(*C);
+    return 0;
 }
-return 0;
+
+/* Fungsi untuk melakukan perkalian 2 matriks */
+void *multiply (void *param)
+{
+    struct v *par = (struct v*) param; // Mengeset objek structure v bernama par dengan param
+    int h; // Variabel looping
+    int i = par -> i; // Mengeset nilai i dengan nilai i pada par
+    int j = par -> j; // Mengeset nilai j dengan nilai j pada par
+    int hasil = 0; // Menunjukkan hasil perhitungan matriks
+    
+    for (h = 0; h < K; h++)
+    {
+        hasil += A[i][h] * B[h][j]; // Hasil didefiniskan dengan menambahkan semua hasil perkalian baris i kolom h dengan baris h kolom j
+        *C[i][j] = hasil; // Mengeset nilai matriks C pada baris i dan kolom j dengan hasil
+    }
+    return(hasil);
 }
