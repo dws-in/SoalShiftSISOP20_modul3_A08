37eb4ff309112670f697d510dfe4af95e3e82da9
dws-in <61307469+dws-in@users.noreply.github.com>
2020-04-11 19:56:21 +0700

    Update 4b.c

--- soal4/4b.c
+++ soal4/4b.c
@@ -1,60 +1,72 @@
-#include<stdio.h>
-#include<string.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
 
-unsigned long long total[10][10];
-pthread_t thread[500];
-int status;
-int (*hasil) [10];
+void *addition(void* arg) 
+{ 
+    int *n = (int *)arg;
+    int a = *n;
+    int result = 0;
 
-void *factorial(void *num){
-	key_t key = 8808;
-   	int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-   	hasil = shmat(shmid, 0, 0);
-	
-	if(shmid<0)
-	{
-		perror("shmget");
-		exit(1)
-	)
-	
-	int h,i,j;
-	long long ans = 0;
-	//int fak = *((int *)num);
-	pthread_t id = pthread_self();
-	for (i = 0; i <= 3; i++){
-		for(j=0; j<=4; j++){
-			jumlah = hasil[i][j];
-			for(k=0;k<=jumlah;k++){			
-				ans = ans + i;
-				total[i][j] = ans;
-	}
-	while (!pthread_equal(id, thread[status]));
-	printf("%lld\n", total[i][j]);
-	status++;
+    for(int i=1;i<=a;i++){
+        result += i;
+    }
+    int *res = (int*)malloc(sizeof(int));
+    *res = result;
 }
 
-int main ()
-{	
-	key_t key = 8808;
-	int shmid = shmget (key, sizeof(int[10][10]), IPC_CREAT | 0666);
-	hasil = shmat(shmid,0,0);
-	int i;
-	int j;
-
-    printf("Hasil perkalian matriks: \n");
-    for(i = 0; i < 4; i++){
-        for(j = 0; j < 5; j++){
-            printf("%d\t", hasil[i][j]);
+int main(){
+    		// shared memory tempelate
+		key_t key=1234;
+		int shmid = shmget(key, sizeof (int[20]), IPC_CREAT | 0666);
+		int (*C)[4][5];
+		C = shmat(shmid,0,0);
+	
+	// ini pokonya cuma copas yang dari 4aa
+	printf("hasil kali :\n");
+    for(int i=0;i<4;i++){
+        for(int j=0;j<5;j++){
+            printf("%d\t", *C[i][j]);
         }
         printf("\n");
     }
+printf("\n");
 
-    printf ("\nHasil sum:\n");
-	//...
+    pthread_t tid[20];
+
+    int count =  0;
+    int *data1 = NULL;
+    for(int i=0;i<4;i++){
+        for(int j=0;j<5;j++){
+            data1 = (int *)malloc(sizeof(int));
+
+            data1 = C[i][j];
+
+            pthread_create(&tid[count], NULL, addition, (void*)(data1));
+	 /* Membuat thread pada array of thread tid pada indeks ke-indeks
+            dengan memanggil fungsi addition dengan parameter data1 */
+            count++;
+        }
+    }
+
+	printf("hasil jumlah per elemen:\n");
+    for(int i=0;i<20;i++){
+        void *k;
+        pthread_join(tid[i], &k); 
+	/*join thread dengan return value k */
+
+        int *p = (int *)k; //return value disimpan dalam variabel p
+        printf("%d\t", *p);
+        if ((i + 1) % 5 == 0){ // tiap 5 elemen, dicut buat dienter.. gitu pokoknya
+            printf("\n");
+        }
+    }
+    
+    shmdt(C);
+    shmctl(shmid, IPC_RMID, NULL);
     return 0;
 }
